83e497cbb4378c5e4210b6e0f8c983de4633e90b
Riley Smith <gordonrileysmith@gmail.com>
2023-08-05 00:21:12 -0600

    deploying

--- server/server.js
+++ server/server.js
@@ -27,11 +27,13 @@ app.use(express.json());
 
 // Serve up static assets
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+  app.use(express.static(path.join(__dirname, 'build', 'index.html')));
 }
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  // res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+
 });
 
 db.once('open', () => {
