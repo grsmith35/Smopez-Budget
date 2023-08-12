8f34ca51a2dae070b4b05e55eed8c17007581c6f
Riley <gordonrileysmith@gmail.com>
2023-08-12 15:44:16 -0600

    lkjasdf

--- server/server.js
+++ server/server.js
@@ -33,8 +33,8 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 app.get('*', (req, res) => {
-  // res.sendFile(path.join(__dirname, '../client/build/index.html'));
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  // res.sendFile(path.join(__dirname, 'build', 'index.html'));
   // res.sendFile(path.join(__dirname + '/dist/YOURPROJECTNAME/index.html'));
 
 });
