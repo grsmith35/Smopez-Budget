2a8818ea286f33f25a02a1fd6e0eef5ee3d9f45b
Riley <gordonrileysmith@gmail.com>
2023-08-12 15:12:44 -0600

    trying change to file location

--- server/server.js
+++ server/server.js
@@ -29,12 +29,14 @@ app.use(express.json());
 if (process.env.NODE_ENV === 'production') {
   // app.use(express.static(path.join(__dirname, 'build', 'index.html')));
   // app.use(express.static('public/client'))
-  app.use(express.static(path.join(__dirname, 'build')));
+  app.use(express.static(path.join('build')));
+  // app.use(express.static(path.join(__dirname, 'build')));
+
 }
 
 app.get('*', (req, res) => {
   // res.sendFile(path.join(__dirname, '../client/build/index.html'));
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile('index.html');
   // res.sendFile(path.join(__dirname + '/dist/YOURPROJECTNAME/index.html'));
 
 });
